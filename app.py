0b5bb8d80c572314ecf8b2bc658f80cfc38bb829
NitishKampati <nitishkampati@gmail.com>
2025-03-31 22:10:43 -0500

    added few imports

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+import os
+from flask import Flask 
 
 app = Flask(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     app.run(debug=True)
